401269f3d9a4313b37d177f7b961f6be9e30238f
Jonathan Martin <hello@jonathanleemartin.com>
2023-07-14 14:56:12 -0400

    Welcome people.

--- index.js
+++ index.js
@@ -10,29 +10,63 @@ const app = new App({
 });
 
 const welcomeChannelId = process.env.SLACK_WELCOME_CHANNEL_ID;
+const welcomeGuideLink = process.env.SLACK_WELCOME_GUIDE_LINK;
 
 // When a user joins the team, send a message in a predefined channel asking them to introduce themselves
-app.event('team_join', async ({ event, client, logger }) => {
+app.event('team_join', async ({ event, client }) => {
   let { user } = event;
+  
+  await sayHello({ event, client, user });
+  await sendOnboarding({ client, user });
+});
 
+app.event('member_joined_channel', async ({ event, client }) => {
+  let { user: userId } = event;
+  let { user } = await client.users.info({ user: userId });
+
+  await sayHello({ event, client, user });
+  await sendOnboarding({ client, user });
+});
+
+async function sendOnboarding({ client, user }) {
+  let { channel } = await client.conversations.open({
+    users: user.id
+  });
+
+  const { message } = await client.chat.postMessage({
+    channel: channel.id,
+    text:
+`Welcome to Market Social! 🎉 Market Social is a social community for young adults in their 20s and 30s living in C’ville. We help people in a transient city form acquaintances that lead to meaningful relationships.
+
+First up, please:
+
+- *Read the <${welcomeGuideLink}|Welcome Guide>* — it’s got all the important things 😎
+- *Say hi in the <#${welcomeChannelId}> channel* (I’ve already started a thread for you 🤗)`
+  });
+  console.log(message);
+}
+
+async function sayHello({ event, client, user }) {
+  console.log(event);
 
   // Ignore @deactivateduser's
   if (user.deleted) {
     return;
   }
 
-  try {
-    // Call chat.postMessage with the built-in client
-    const result = await client.chat.postMessage({
-      channel: welcomeChannelId,
-      text: `Welcome to the team, <@${user.id}>! 🎉 You can introduce yourself in this channel.`
-    });
-    logger.info(result);
-  }
-  catch (error) {
-    logger.error(error);
-  }
-});
+  const { message } = await client.chat.postMessage({
+    channel: welcomeChannelId,
+    text: `Everyone welcome <@${user.id}> to Market Social! 🎉 Say hello in the thread below: 🧵👇`
+  });
+  console.log(message);
+
+  const { message: reply } = await client.chat.postMessage({
+    channel: welcomeChannelId,
+    thread_ts: message.ts,
+    text: `👋 _(just kicking off introductions)_`
+  });
+  console.log(reply);
+};
 
 (async () => {
   // Start the app
